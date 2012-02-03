b6a368a9abaab3358628c58a31863120e8d7d8b2
Umar FERRER <ferrer.umar@gmail.com>
2012-02-03 16:16:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PryusBliss::Application.config.secret_token = 'e5983f403d9f43cad52e4acf7f4e0cd92b1d1026d5d25a3196a89cd9418fe404c1b5f65032a949c833674ed2464d2e255f830b4934d02a3e587908a607f446fa'
